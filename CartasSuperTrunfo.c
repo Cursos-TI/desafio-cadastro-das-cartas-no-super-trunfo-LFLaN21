7306d7d99dc67d4349ea74fd67b3257a36aba9a8
LFLaN21 <riquelmefreire_@hotmail.com>
2025-04-02 18:23:54 +0000

    Acrescimo de if else por decisão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 
-int main()
-{
+int main() {
 
-    char estado1, estado2[15];
-    char codigoDaCarta1, codigoDaCarta2[20];
-    char nomeDaCidade1, nomeDaCidade2[20];
+    char estado1, estado2;
+    char codigoDaCarta1 [20], codigoDaCarta2[20];
+    char nomeDaCidade1 [20], nomeDaCidade2[20];
     int populacao1, populacao2, pontoTuristico1, pontoTuristico2;
-    float PIB1, PIB2, areakm1, areakm2;
+    float PIB1, PIB2, areakm1, areakm2, densidadePopulacional1, densidadePopulacional2, PIBperCapita1, PIBperCapita2;
 
     printf("\n***SUPER TRUNFO***\n");
 
@@ -26,7 +25,7 @@ int main()
     printf("População:\n");
     scanf("%d", &populacao1);
 
-    printf("Área:\nkm²");
+    printf("Área km²:\n");
     scanf("%f", &areakm1);
 
     printf("PIB:\n");
@@ -49,7 +48,7 @@ int main()
     printf("População:\n");
     scanf("%d", &populacao2);
 
-    printf("Área:\nkm²");
+    printf("Área km²:\n");
     scanf("%f", &areakm2);
 
     printf("PIB:\n");
@@ -57,6 +56,28 @@ int main()
 
     printf("Insira a quantidade de pontos turísticos:\n");
     scanf("%i", &pontoTuristico2);
+
+//Calculo da densidade populacional e o PIB per capita.
+    densidadePopulacional1 = populacao1 / areakm1;
+    densidadePopulacional2 = populacao2 / areakm2;
+    PIBperCapita1 = PIB1 / populacao1;
+    PIBperCapita2 = PIB2 / populacao2;
+
+
+    printf("Densidade populacional da cidade 1: %.2f\n", densidadePopulacional1);
+    printf("Densidade populacional da cidade 2: %.2f\n", densidadePopulacional2);
+    printf("PIB per capita da cidade 1: %.2f\n", PIBperCapita1);
+    printf("PIB per capita da cidade 2: %.2f\n", PIBperCapita2);
+
+    if (densidadePopulacional1 > densidadePopulacional2) {
+        printf("A carta 2 Venceu!\n");
+    } else if (densidadePopulacional1 < densidadePopulacional2) {
+        printf("A carta 1 Venceu!\n");
+    } else {
+        printf("\n***EMPATOU***\n");
+    }
+
+
 
     return 0;
 }
